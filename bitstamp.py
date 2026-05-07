7440cd7646ced95753836c49209bda3e3daaa856
Gabriel <bielbielaguiar@gmail.com>
2026-05-07 15:07:54 -0300

    initial commit

---
+++ bitstamp.py
@@ -0,0 +1,50 @@
+import json
+
+import websocket
+
+def comprar():
+    pass
+
+def vender():
+    pass
+
+def on_message(ws, message):
+    parsed_message = json.loads(message)
+
+    print("-" * 50)
+    #print(parsed_message)
+    if 'id' in parsed_message['data']:
+        price = parsed_message['data']['price']
+        amount_sold = parsed_message['data']['amount']
+        amount_sold_str = parsed_message['data']['amount_str']
+
+        print(f"BTC Price: ${price}")
+        print(f"Amount Sold: {amount_sold_str}")
+        print(f"Price of {amount_sold_str} BTC: ${price * amount_sold:.2f}")
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws, close_status_code, close_message):
+    print("*--- closed connection ---*")
+    if close_status_code:
+        print(close_status_code)
+    if close_message:
+        print(close_message)
+
+def on_open(ws):
+    print("*--- opened connection ---*")
+
+    json_subscribe = """
+    {
+        "event": "bts:subscribe",
+        "data": {
+            "channel": "live_trades_btcusd"
+        }
+    }
+    """
+    ws.send(json_subscribe)
+
+if __name__ == "__main__":
+    ws = websocket.WebSocketApp("wss://ws.bitstamp.net", on_open=on_open, on_close=on_close, on_message=on_message, on_error=on_error)
+    ws.run_forever()
